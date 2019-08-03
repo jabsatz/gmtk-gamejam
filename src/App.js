57bed59d08f59dfa20d15605d5ba9325c53a4e7f
Narmuzz <narmuzz95@gmail.com>
2019-08-02 22:10:18 -0300

    Base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: lightgray;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Bar = styled.div`
+  height: 100px;
+  background: red;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Bar />
+    </Wrapper>
   );
 }
 
